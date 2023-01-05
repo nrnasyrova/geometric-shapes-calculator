209a575bba0559b5718e9e72e3d8be0361a5bea8
nrnasyrova <65603709+nrnasyrova@users.noreply.github.com>
2023-01-05 14:57:07 +0500

    added ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# geometric-shapes-calculator
+# Geometric Shapes Calculator
+
+This library provides functionality to calculate area of all geometric shapes.
+Currently, it consists of two implemented shapes:
+1. Circle
+2. Triangle
+
+Moreover, it includes interfaces:
+
+- ICircle - interface for Circle class that includes CalculateDiameter method
+- ITriangle - interface for Triangle class that includes a method to check if a triangle is right and a method to calculate perimeter
+
+Furthermore, one can extend a library and add additional geometric shapes and methods by implementing IShape interface.
---
+++ query.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE Products (
+                          Id INT PRIMARY KEY,
+                          "Name" TEXT
+);
+
+INSERT INTO Products
+VALUES
+    (1, 'Apples'),
+    (2, 'Milk'),
+    (3, 'Tomato');
+
+CREATE TABLE Categories (
+                            Id INT PRIMARY KEY,
+                            "Name" TEXT
+);
+
+INSERT INTO Categories
+VALUES
+    (1, 'Fruits'),
+    (2, 'Dairy_produce'),
+    (3, 'Vegetables');
+
+CREATE TABLE ProductCategories (
+                                   ProductId INT FOREIGN KEY REFERENCES Products(Id),
+                                   CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+                                   PRIMARY KEY (ProductId, CategoryId)
+);
+
+INSERT INTO ProductCategories
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+SELECT P."Name" as ProductName, C."Name" as CategoryName
+FROM Products P
+         LEFT JOIN ProductCategories PC
+                   ON P.Id = PC.ProductId
+         LEFT JOIN Categories C
+                   ON PC.CategoryId = C.Id;
